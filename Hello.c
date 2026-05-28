55f3ac714ca7d239622b805bb0a3ca4a9e398e57
Ganesh Hegde <hegdeganesh186@gmail.com>
2026-05-28 10:41:21 +0530

    Constants

--- Hello.c
+++ Hello.c
@@ -2,8 +2,25 @@
 #include <stdio.h>
 
 int main(void) {
-    char name[100];
+int n;
+long int b;
+float p;
+long double s;
+char q;
+
+    char name[67];
     printf("What's your name? ");
-    fgets(name, sizeof(name), stdin);
-    printf("hello, %s", name);
+    scanf("%s",name);
+    printf("hello, %s\n", name);
+printf("Size of n is:%lu\n",sizeof(n));
+printf("Size of p is:%lu\n",sizeof(p));
+printf("Size of s is:%lu\n",sizeof(s));
+printf("Size of b is:%lu\n",sizeof(b));
+printf("Size of q is:%lu\n",sizeof(q));
+printf("Size of name is:%lu\n",sizeof(name));
+printf("%d\n",printf("Hello World\n"));
+printf("%d\n",printf("%s","Hello World\n"));
+
+
+return 0;
 }
---
+++ constants.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#define PI 3.145
+int main(){
+printf("PI value is %.3f\n",PI);
+return 0;
+}
