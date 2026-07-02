56fa8800026674ab49e25dc55d2fcba79585434c
Ganesh Hegde <hegdeganesh186@gmail.com>
2026-07-02 21:12:11 +0530

    3X3 Matrix Multiplication

---
+++ matrixmul.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(void){
+int n=3;
+int a[3][3]={{1,2,3},{4,5,6},{2,3,4}};
+int b[3][3]={{7,8,9},{1,2,3},{3,4,4}};
+int c[3][3]={0};
+printf("Matrix Multiplication:\n");
+for(int i=0;i<n;i++){
+for(int j=0;j<n;j++){
+c[i][j]=0;
+for(int k=0;k<n;k++){
+c[i][j]=c[i][j]+a[i][k]*b[k][j];
+}
+}
+}
+for(int i=0;i<n;i++){
+for(int j=0;j<n;j++){
+printf("%d ",c[i][j]);
+}
+printf("\n");
+}
+return 0;
+}
+
