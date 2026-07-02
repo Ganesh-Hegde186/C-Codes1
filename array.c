7e1f1b617957dd00e868400c707c85c69302bd4c
Ganesh Hegde <hegdeganesh186@gmail.com>
2026-07-02 17:36:14 +0530

    Basic arrays

--- array.c
+++ array.c
@@ -1,14 +1,36 @@
 #include<stdio.h>
 #include<ctype.h>
+int sum(int *array,int p);
+float avg(int *ar,int x);
 int main(void){
-const int N=5;
-int arr[N]={10,20,30};
-printf("%i %i %i %i\n",arr[0],arr[1],arr[2],arr[3],arr[4]);
-printf("Size of the array is:%zu\n",sizeof(arr[0]));
+int arr[5]={10,20,30};
+printf("Size of the array element is:%zu\n",sizeof(arr[0]));
 printf("Size of arr is:%zu\n",sizeof(arr));
-printf("%zu\n",sizeof(arr)/sizeof(arr[0]));
-for(int i=0;i<N;i++){
-printf("%i ",arr[i]);
+printf("Number of Elements:%zu\n",sizeof(arr)/sizeof(arr[0]));
+for(int i=0;i<5;i++){
+printf("%i\n",arr[i]);
 }
+for(int i=0;i<5;i++){
+printf("%p\n",(void*)&arr[i]);
+}
+int n= sizeof(arr)/sizeof(arr[0]);
+printf("Sum of array is:%d\n",sum(arr,n));
+printf("Avearge of array is:%f\n",avg(arr,n));
 return 0;
+}
+int sum(int *array,int p){
+int s=0;
+for(int i=0;i<p;i++){
+s=s + *(array+i);
+}
+return s;
+}
+float avg(int *ar,int x){
+float a=0;
+float b=0;
+for(int i=0;i<x;i++){
+a = a + *(ar+i);
+}
+b= a/x;
+return b;
 }
